11261f262a5e8bea384f7f1682eb6e226ec623a4
cozdemir <cengiz.ozdemir@assistt.com.tr>
2023-06-02 10:41:26 +0300

    getData - CoreData

--- TravelBook/ListVliewController.swift
+++ TravelBook/ListVliewController.swift
@@ -6,10 +6,13 @@
 //
 
 import UIKit
+import CoreData
 
 class ListVliewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    var titleArraty = [String]()
+    var idArray = [UUID]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,18 +21,51 @@ class ListVliewController: UIViewController, UITableViewDelegate, UITableViewDat
 
         tableView.delegate = self
         tableView.dataSource = self
+        getData()
+        
     }
+    func getData() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Places")
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(request)
+            if results.count > 0 {
+                self.titleArraty.removeAll(keepingCapacity: false)
+                self.idArray.removeAll(keepingCapacity: false)
+                
+                for result in results as! [NSManagedObject] {
+                    if let title = result.value(forKey: "title") as? String {
+                        self.titleArraty.append(title)
+                    }
+                    if let id = result.value(forKey: "id") as? UUID {
+                        self.idArray.append(id)
+                    }
+                    tableView.reloadData()
+                }
+            }
+            
+        } catch {
+            print("error")
+        }
+    }
+    
+    
+    
     @objc func addButtonClicked() {
         performSegue(withIdentifier: "toViewController", sender: nil)
     }
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return titleArraty.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "test"
+        cell.textLabel?.text = titleArraty[indexPath.row]
         return cell
     }
 
